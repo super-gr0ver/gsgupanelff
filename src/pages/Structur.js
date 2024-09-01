a5c7da176215891580fd4c4c9e018ace9ca62436
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 20:35:19 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -2,17 +2,8 @@ import React from "react";
 
 export function Structur() {
   return (
-    // <div className='main-page main-container structur'>
-    //   <a href='https://gukolomna.ru/obrazovanie/departments/'>Факультеты</a>
-    //   <a href='https://gukolomna.ru/digitalfc/'>Цифровой портрет факультетов</a>
-    //   <a href='https://gukolomna.ru/obrazovanie/kafedry/'>Кафедры</a>
-    // </div>
     <div className="main-page main-container structur">
-      <iframe
-        style="border: none"
-        src="https://gukolomna.ru/studentu/schedule/"
-        sandbox="allow-scripts allow-same-origin"
-      ></iframe>
+      <a href="https://gukolomna.ru/obrazovanie/departments/">Факультеты</a>
     </div>
   );
 }
