f13768fb1a49e6058647358b68a91d697d4dd757
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 20:22:36 +0300

    fix

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,30 +1,31 @@
 import React from "react";
-import Swiper core and required modules
-import { A11y, Autoplay, Navigation, Pagination } from 'swiper';
+// import Swiper core and required modules
+import { A11y, Autoplay, Navigation, Pagination } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/autoplay';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import info1 from '../img/info/1.jpg';
-import info2 from '../img/info/2.jpg';
-import info3 from '../img/info/3.jpg';
+import info1 from "../img/info/1.jpg";
+import info2 from "../img/info/2.jpg";
+import info3 from "../img/info/3.jpg";
 
 const images = [info1, info2, info3];
 
 export function Info() {
   return (
-    <div className='main-page'>
+    <div className="main-page">
       <iframe
         style="border: none"
         src="https://gukolomna.ru/studentu/schedule/"
         sandbox="allow-scripts allow-same-origin"
       ></iframe>
-      {/* <Swiper
+
+      <Swiper
         // install Swiper modules
         modules={[Navigation, Pagination, Autoplay, A11y]}
         loop={true}
@@ -37,15 +38,15 @@ export function Info() {
         spaceBetween={30}
         navigation={true}
         pagination={{
-          type: 'fraction',
+          type: "fraction",
         }}
       >
         {images.map((img, i) => (
           <SwiperSlide key={i}>
-            <img src={img} alt={`info${i + 1}`} className='slide' />
+            <img src={img} alt={`info${i + 1}`} className="slide" />
           </SwiperSlide>
         ))}
-      </Swiper> */}
+      </Swiper>
     </div>
   );
 }
