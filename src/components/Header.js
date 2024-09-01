83f08d448241b98fff4715d84939cdb84043d747
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 19:36:45 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from '../img/gsgu-logo.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import logo from "../img/gsgu-logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className='main-header'>
-      <Link to='/'>
-        <img height={150} width={150} src={logo} alt='GSGU logo' />
+    <header className="main-header">
+      <Link to="/">
+        <img height={150} width={150} src={logo} alt="GSGU logo" />
       </Link>
 
       <h1>Филологический факультет</h1>
+      <h1>Государственный социально-гуманитарный университет</h1>
     </header>
   );
 }
