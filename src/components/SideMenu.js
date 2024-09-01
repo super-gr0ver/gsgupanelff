36d75f7c6c1bc9b86e0a12e5978aaf7b765e6982
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 21:26:07 +0300

    fix

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -10,13 +10,13 @@ function SideMenu() {
         <NavLink to="/">Главная</NavLink>
         {/* <NavLink to="info">Расписание</NavLink> */}
         <NavLink to="structur">Расписание</NavLink>
-        <NavLink to="stubpage">Учебный процесс</NavLink>
-        <NavLink to="stubpage">Документы</NavLink>
+        <NavLink to="eduprocess">Учебный процесс</NavLink>
+        <NavLink to="docs">Документы</NavLink>
         {/* <NavLink to="abitur">Абитуриенту</NavLink> */}
-        <NavLink to="stubpage">Проекты</NavLink>
-        <NavLink to="stubpage">Технологии</NavLink>
-        <NavLink to="stubpage">Трудоустройство</NavLink>
-        <NavLink to="stubpage">Контакты</NavLink>
+        <NavLink to="projects">Проекты</NavLink>
+        <NavLink to="tech">Технологии</NavLink>
+        <NavLink to="educuality">Трудоустройство</NavLink>
+        <NavLink to="contact">Контакты</NavLink>
       </div>
       <div className="socials-container">
         {/* <div className='socials-item'>
--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -1,40 +1,34 @@
-import React from 'react';
-// import Swiper core and required modules
-import { A11y, Autoplay, Navigation, Pagination } from 'swiper';
+// import React from 'react';
+// import { A11y, Autoplay, Navigation, Pagination } from 'swiper';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/autoplay';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper/css';
+// import 'swiper/css/autoplay';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
-import docs1 from '../img/docs/1.jpg';
+// import docs1 from '../img/docs/1.jpg';
+
+// export function Docs() {
+//   return (
+//     <div className='main-page'>
+//       <Swiper
+//       modules={[Navigation, Pagination, Autoplay, A11y]}
+//       slidesPerView={1}
+//       >
+//         <SwiperSlide>
+
+//           <img src={docs1} alt='docs1' className='slide' />
+//         </SwiperSlide>
+//       </Swiper>
+//     </div>
+//   );
+// }
+import React from "react";
 
 export function Docs() {
   return (
-    <div className='main-page'>
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Autoplay, A11y]}
-        // loop={true}
-        slidesPerView={1}
-        // speed={1000}
-        // autoplay={{
-        //   delay: 5000,
-        //   disableOnInteraction: false,
-        // }}
-        // spaceBetween={30}
-        // navigation={true}
-        // pagination={{
-        //   type: 'fraction',
-        // }}
-      >
-        <SwiperSlide>
-          <img src={docs1} alt='docs1' className='slide' />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <div className="main-page main-container structur">В разработке ...</div>
   );
 }
