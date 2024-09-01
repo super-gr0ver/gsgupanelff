50155e3925b084fb5c3dd01c88882bafbf2903b3
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 21:00:45 +0300

    fix

--- src/pages/Stubpage.js
+++ src/pages/Stubpage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function Structur() {
+export function Stubpage() {
   return (
     <div className="main-page main-container structur">В разработке ...</div>
   );
