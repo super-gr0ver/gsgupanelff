044313fcc98fd3e7be877515599d58132c3fe569
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 20:38:08 +0300

    fix

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -4,6 +4,11 @@ export function Structur() {
   return (
     <div className="main-page main-container structur">
       <a href="https://gukolomna.ru/obrazovanie/departments/">Факультеты</a>
+      <iframe
+        style="border: none"
+        src="https://gukolomna.ru/studentu/schedule/"
+        sandbox="allow-scripts allow-same-origin"
+      ></iframe>
     </div>
   );
 }
